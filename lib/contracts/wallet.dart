01233d46eab67f64dbbe8cb7e4072c12a27ee581
Andrew Wahid <andrew.wahid1@gmail.com>
2022-11-23 22:42:28 +0200

    Support multi send call contracts

--- lib/contracts/wallet.dart
+++ lib/contracts/wallet.dart
@@ -2,6 +2,7 @@ import 'dart:typed_data';
 
 import 'package:wallet_dart/constants/constants.dart';
 import 'package:wallet_dart/contracts/factories/EIP4337Manager.g.dart' as w;
+import 'package:wallet_dart/contracts/factories/MultiSendCallOnly.g.dart';
 import 'package:wallet_dart/contracts/factories/SafeProxy.g.dart';
 import 'package:wallet_dart/contracts/factories/SocialRecoveryModule.g.dart';
 import 'package:web3dart/web3dart.dart';
@@ -11,6 +12,7 @@ class CWallet {
 
   static w.EIP4337Manager interface = w.EIP4337Manager(address: address, client: Constants.web3client);
   static SafeProxy proxyInterface = SafeProxy(address: EthereumAddress(Uint8List(20)), client: Constants.web3client);
+  static MultiSendCallOnly multiSendCallOnlyInterface = MultiSendCallOnly(address: EthereumAddress(Uint8List(20)), client: Constants.web3client);
   static w.EIP4337Manager customInterface(EthereumAddress address, {Web3Client? client}) => w.EIP4337Manager(address: address, client: client ?? Constants.web3client);
   static SocialRecoveryModule recoveryInterface(EthereumAddress address, {Web3Client? client}) => SocialRecoveryModule(address: address, client: client ?? Constants.web3client);
 }
