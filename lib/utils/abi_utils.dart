fc4117aaae32bfbe3619df1c9e5bfae79e3d0702
Andrew Wahid <andrew.wahid1@gmail.com>
2022-12-13 15:41:02 +0200

    Feat/add decodeAbi

--- lib/utils/abi_utils.dart
+++ lib/utils/abi_utils.dart
@@ -13,3 +13,13 @@ Uint8List encodeAbi(List<String> types, List<dynamic> values){
   TupleType(abiTypes).encode(values, result);
   return result.asBytes();
 }
+
+List<dynamic> decodeAbi(List<String> types, Uint8List value){
+  List<AbiType> abiTypes = [];
+  for (String type in types){
+    var abiType = parseAbiType(type);
+    abiTypes.add(abiType);
+  }
+  final parsedData = TupleType(abiTypes).decode(value.buffer, 0);
+  return parsedData.data;
+}
