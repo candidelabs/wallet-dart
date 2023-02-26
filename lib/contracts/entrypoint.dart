b322dbec1c446141c56280a8b802b3445b6fbd4c
Andrew Wahid <andrew.wahid1@gmail.com>
2023-02-27 00:18:40 +0200

    refactor: remove unused variables

--- lib/contracts/entrypoint.dart
+++ lib/contracts/entrypoint.dart
@@ -1,9 +1,9 @@
+import 'dart:typed_data';
+
 import 'package:wallet_dart/contracts/factories/EntryPoint.g.dart';
 import 'package:web3dart/web3dart.dart';
 
 class IEntrypoint {
-  static EthereumAddress address = EthereumAddress.fromHex("0xC1c7Ff116Acd223426a50De4d4Bb7117D0c387Ff");
-
   static EntryPoint interface({EthereumAddress? address, required Web3Client client}) =>
-      EntryPoint(address: address ?? IEntrypoint.address, client: client);
+      EntryPoint(address: address ?? EthereumAddress(Uint8List(20)), client: client);
 }
